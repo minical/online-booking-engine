aee5941bdc7653319ecea570b38e076e165a61e2
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2022-08-22 10:28:14 +0530

    get whitelabel by username (case-sensitive)

--- controllers/online_reservation.php
+++ controllers/online_reservation.php
@@ -80,7 +80,7 @@ class Online_reservation extends MY_Controller
             $white_label_name = $white_label_name[0];
         }
         $data['whitelabel_detail'] = '';
-        $white_label_detail = $this->Whitelabel_partner_model->get_partners(array('username' => $white_label_name));
+        $white_label_detail = $this->Whitelabel_partner_model->get_partner_by_username($white_label_name);
         if($white_label_detail)
         {
             $white_label_detail = $white_label_detail[0];
